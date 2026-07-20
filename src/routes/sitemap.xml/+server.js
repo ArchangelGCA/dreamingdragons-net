bd20c4d12505de3997c755072adffd8f8d35386b
ArchangelGCA <archangelgca@gmail.com>
2026-07-20 12:14:52 +0200

    Changelogs: - Version bump. - Updated dependencies. - Minor changes.

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,4 +1,4 @@
-import * as sitemap from 'super-sitemap';
+import * as sitemap from 'super-sitemap/sveltekit';
 
 export const prerender = true;
 
@@ -6,7 +6,7 @@ export const GET = async ({ url }) => {
   return await sitemap.response({
     origin: 'https://dreamingdragons.net',
     excludeRoutePatterns: [
-      '^/admin.*',
+      /^\/admin.*/,
     ],
     defaultChangefreq: 'weekly',
     defaultPriority: 0.7,
